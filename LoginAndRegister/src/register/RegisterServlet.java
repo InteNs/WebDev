959239a8207ca04760522303292351a879a02956
RyuuMasato <jorritmeulenbeld@icloud.com>
2015-04-22 10:32:19 +0200

    'debiele-spelfout-verbeterd'

--- LoginAndRegister/src/register/RegisterServlet.java
+++ LoginAndRegister/src/register/RegisterServlet.java
@@ -14,7 +14,7 @@ public class RegisterServlet extends HttpServlet {
 		String password = req.getParameter("passwordreg");
 		String userName = req.getParameter("username");
 		String firstName = req.getParameter("name");
-		String sirName = req.getParameter("sirname");
+		String sirName = req.getParameter("surname");
 		String address = req.getParameter("address");
 		String country = req.getParameter("country"); 
 		boolean succes = false;
